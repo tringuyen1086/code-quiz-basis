ef94624f4712f017909a72472592f796fc2b90f6
Tri Nguyen <tri.nguyen1086@gmail.com>
2021-11-27 01:46:42 -0800

    upload files

---
+++ js/questions.js
@@ -0,0 +1,205 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "JavaScript File has an extension of?",
+    choices: [
+      ".java",
+      ".js",
+      ".javascript",
+      "xml"
+    ],
+    answer: ".js"
+  },
+  {
+    title: "Why JavaSript and Java have similar name?",
+    choices: [
+      "JavaSript is a stripped-down version of Java",
+      "JavaScript's syntax is loosely based on Java's",
+      "Both of the above",
+      "None of the above"
+    ],
+    answer: "JavaScript's syntax is loosely based on Java's"
+  },
+  {
+    title: "Which of the following can't be done with client-side JavaSript?",
+    choices: [
+      "Validating a form",
+      "Sending a form's contents by email",
+      "Storing the form's contents to a database file on the server",
+      "None of the above"
+    ],
+    answer: "Storing the form's contents to a database file on the server"
+  },
+  {
+    title: "Which of the following attribute can hold the JavaScript version?",
+    choices: [
+      "LANGUAGE",
+      "SCRIPT",
+      "VERSION",
+      "None of the above"
+    ],
+    answer: "LANGUAGE"
+  },
+  {
+    title: "JavaScript entities start with _______ and end with _________?",
+    choices: [
+      "Semicolon, colon",
+      "Semicolon, Ampersand",
+      "Ampersand, colon",
+      " Ampersand, semicolon"
+    ],
+    answer: " Ampersand, semicolon"
+  },
+  {
+    title: "Which of the following best describes JavaScript?",
+    choices: [
+      "A low-level programming language",
+      "A scripting language precompiled in the browser",
+      "A compiled scripting language",
+      "An object-oriented scripting language"
+    ],
+    answer: "An object-oriented scripting language"
+  },
+  {
+    title: "Which one is a looping structure in JavaScript?",
+    choices: [
+      "All the below",
+      "For",
+      "While",
+      "do-while loops"
+    ],
+    answer: "All the below"
+  },
+  {
+    title: "JavaScript is interpreted by _________?",
+    choices: [
+      "Client",
+      "Server",
+      "Object",
+      "None of the above"
+    ],
+    answer: "Client"
+  },
+  {
+    title: "Which of the following is not considered a JavaScript operator",
+    choices: [
+      "new",
+      "this",
+      "delete",
+      "typeof"
+    ],
+    answer: "this"
+  },
+  {
+    title: "Using _______ statement is how you test for a specific condition",
+    choices: [
+      "Select", 
+      "If", 
+      "Switch",
+      "For"
+    ],
+    answer: "If"
+  },
+  {
+    title: "What are the two basic groups of data types in JavaScript?",
+    choices: [
+      "Primitive and attribute",
+      "Primitive and reference types",
+      "Reference types and attribute",
+      "None of the above"
+    ],
+    answer: "Primitive and reference types"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers"
+    ],
+    answer: "alerts"
+  },
+  {
+    title: "Boolean operators that can be used in JavaScript include:",
+    choices: [
+      "'And' Operator &&",
+      "'Or' Operator ||",
+      "'Not' Operator !",
+      "All the above"
+    ],
+    answer: "All the above"
+  },
+  {
+    title:
+      "Which one of these is not among the three different types of errors in JavaScript?",
+    choices: [
+      "Animation time errors",
+      "Load time errors",
+      "Run time errors",
+      "Logical Errors"
+    ],
+    answer: "Animation time errors"
+  },
+  {
+    title: "What is the data type of variables in JavaScript?",
+    choices: [
+      "Object data types",
+      "Function data type",
+      "None of the above",
+      "All of the above"
+    ],
+    answer: "Object data types"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: [
+      "quotes",
+      "curly brackets",
+      "parentheses",
+      "square brackets"
+    ],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "All of the above"
+    ],
+    answer: "All of the above"
+  },
+  {
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: [
+      "commas",
+      "curly brackets",
+      "quotes",
+      "parentheses"
+    ],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: [
+      "JavaScript",
+      "terminal / bash",
+      "for loops",
+      "console.log"
+    ],
+    answer: "console.log"
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: [
+      "Alert",
+      "Confirm",
+      "Prompt",
+      "All the above"
+    ],
+    answer: "All the above"
+  }
+];
